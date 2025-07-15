c04cfed25c21100a4e97ac3ed3c716320a7cfbf4
Ismael Tesisteco <ismael.tesisteco@gmail.com>
2025-07-15 22:11:29 +0700

    testing the cicd

--- training_service/app/job_launcher.py
+++ training_service/app/job_launcher.py
@@ -1,6 +1,6 @@
 import uuid
 from google.cloud import run_v2
-from google.cloud.run_v2.types import Job, TaskTemplate, Container
+from google.cloud.run_v2.types import Job, TaskTemplate, Container, ExecutionTemplate
 
 from app.config import PARENT
 
@@ -9,14 +9,15 @@ def launch_training_job(image_uri: str, args: list[str]) -> str:
     job_id = f"train-job-{uuid.uuid4().hex[:8]}"
 
     job = Job(
-        template=run_v2.ExecutionTemplate(
+        template=ExecutionTemplate(
             task_count=1,
             template=TaskTemplate(
                 containers=[Container(image=image_uri, args=args)],
                 max_retries=1,
                 timeout={"seconds": 3600},
+                service_account="terraform-deployer@sodium-pager-461309-p3.iam.gserviceaccount.com",
             ),
-        ),
+        )
     )
 
     operation = client.create_job(parent=PARENT, job=job, job_id=job_id)
