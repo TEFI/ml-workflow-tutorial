646552879be522cb123b0e23cc6b1fb06c35ed95
Ismael Tesisteco <ismael.tesisteco@gmail.com>
2025-07-15 22:58:32 +0700

    testing the cicd

---
+++ ml_pipeline/training.py
@@ -0,0 +1,73 @@
+# training.py
+
+import os
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import joblib
+import mlflow
+from google.cloud import storage
+
+
+def train_model(args):
+    mlflow.set_tracking_uri("file:/tmp/mlruns")
+    mlflow.start_run()
+
+    # Cargar CSV (GCS o local)
+    if args.gcs_path.startswith("gs://"):
+        import gcsfs
+        fs = gcsfs.GCSFileSystem()
+        with fs.open(args.gcs_path) as f:
+            data = pd.read_csv(f)
+    else:
+        data = pd.read_csv(args.gcs_path)
+
+    # Preprocesamiento
+    X = data.drop(columns=["Survived"])
+    y = data["Survived"]
+    for col in ["Name", "Ticket", "Cabin"]:
+        if col in X.columns:
+            X = X.drop(columns=[col])
+    X = pd.get_dummies(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # Entrenamiento
+    clf = RandomForestClassifier(
+        n_estimators=args.n_estimators,
+        max_depth=args.max_depth,
+        min_samples_split=args.min_samples_split,
+        min_samples_leaf=args.min_samples_leaf
+    )
+    clf.fit(X_train, y_train)
+    accuracy = clf.score(X_test, y_test)
+
+    # Log en MLflow
+    mlflow.log_param("n_estimators", args.n_estimators)
+    mlflow.log_param("max_depth", args.max_depth)
+    mlflow.log_param("min_samples_split", args.min_samples_split)
+    mlflow.log_param("min_samples_leaf", args.min_samples_leaf)
+    mlflow.log_param("dataset_path", args.gcs_path)
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Guardar modelo local
+    local_model_path = "random_forest_model.joblib"
+    joblib.dump(clf, local_model_path)
+    mlflow.sklearn.log_model(clf, "model")
+
+    # Subir a GCS
+    model_filename = os.path.basename(args.gcs_path).replace(".csv", "_model.joblib")
+    destination_path = f"models/{model_filename}"
+    upload_to_gcs(local_model_path, f"gs://ml-artifacts-tutorial/{destination_path}")
+
+    mlflow.end_run()
+
+
+def upload_to_gcs(local_file: str, gcs_uri: str):
+    if not gcs_uri.startswith("gs://"):
+        raise ValueError("Destination GCS path must start with 'gs://'")
+
+    bucket_name, blob_path = gcs_uri.replace("gs://", "").split("/", 1)
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(blob_path)
+    blob.upload_from_filename(local_file)
