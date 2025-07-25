d3cb419135ac4cf64728455be5600cc0c0390b3a
Ismael Tesisteco <ismael.tesisteco@gmail.com>
2025-07-25 23:11:55 +0700

    update mlflow uri

--- services/model-server/app/config.py
+++ services/model-server/app/config.py
@@ -1,4 +1,4 @@
 import os
 
-MLFLOW_URI = os.getenv("MLFLOW_URI", "http://35.225.64.219:5000")
+MLFLOW_URI = os.getenv("MLFLOW_URI", "http://34.29.28.17:5000")
 MODEL_URI = os.getenv("MODEL_URI", "models:/RandomForestModelv1@production")
