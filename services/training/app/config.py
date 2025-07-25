3a85e2db0b23bfaff27e01121521820e8a60f36f
Ismael Tesisteco <ismael.tesisteco@gmail.com>
2025-07-25 22:09:35 +0700

    test cicd

--- services/training/app/config.py
+++ services/training/app/config.py
@@ -4,5 +4,5 @@ PROJECT_ID = os.getenv("PROJECT_ID", "sodium-pager-461309-p3")
 REGION = os.getenv("REGION", "us-central1")
 BUCKET_NAME = os.getenv("BUCKET_NAME", "ml-artifacts-tutorial")
 PARENT = f"projects/{PROJECT_ID}/locations/{REGION}"
-SERVICE_ACCOUNT = os.getenv("SERVICE_ACCOUNT", "terraform-deployer@{PROJECT_ID}.iam.gserviceaccount.com")
+SERVICE_ACCOUNT = os.getenv("SERVICE_ACCOUNT", "terraform-deployer@sodium-pager-461309-p3.iam.gserviceaccount.com")
 IMAGE_URI = os.getenv("IMAGE_URI", "us-central1-docker.pkg.dev/sodium-pager-461309-p3/trainer/training-job:latest")
