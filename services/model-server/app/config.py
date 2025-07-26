718cf2cbf861a925b29c511cafc3e681f565320b
Ismael Tesisteco <ismael.tesisteco@gmail.com>
2025-07-26 18:52:45 +0700

    test cicd model server

--- services/model-server/app/config.py
+++ services/model-server/app/config.py
@@ -1,4 +1,4 @@
 import os
 
-MLFLOW_URI = os.getenv("MLFLOW_URI", "http://34.63.176.60:5000")
+MLFLOW_URI = os.getenv("MLFLOW_URI", "http://146.148.62.66:5000")
 MODEL_URI = os.getenv("MODEL_URI", "models:/RandomForestModelv1@production")
